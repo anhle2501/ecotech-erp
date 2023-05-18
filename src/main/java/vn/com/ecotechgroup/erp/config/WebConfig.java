ce7b621cdb35d64f4ddf4997e50ad2807075f753
nhut anh <nhut.anh2501@gmail.com>
2023-05-18 15:09:55 +0700

    add crud for customer no test

--- src/main/java/vn/com/ecotechgroup/erp/config/WebConfig.java
+++ src/main/java/vn/com/ecotechgroup/erp/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/error").setViewName("error");
 	}
 
 	
--- src/main/java/vn/com/ecotechgroup/erp/controller/CustomerController.java
+++ src/main/java/vn/com/ecotechgroup/erp/controller/CustomerController.java
@@ -1,63 +1,103 @@
 package vn.com.ecotechgroup.erp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import vn.com.ecotechgroup.erp.entity.Customer;
 import vn.com.ecotechgroup.erp.repository.CustomerRepository;
-
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
 	CustomerRepository customerRepo;
 
-	@ModelAttribute(name="customer")
+	@ModelAttribute(name = "customer")
 	public Customer customer() {
 		return new Customer();
 	}
-	
+
 	@GetMapping
 	public String showCustomerList(Model model) {
 		List<Customer> customerList = customerRepo.findAll();
 		model.addAttribute("customer", customerList);
+		model.addAttribute("isList", true);
 		return "page/customer";
+	}
+
+	@GetMapping("/{id}/show")
+	public String showCustomer(@PathVariable("id") int id, Model model) {
+		Optional<Customer> customerObj = customerRepo.findById(id);
+		if (customerObj.isPresent()) {
+			model.addAttribute("customer", customerObj);
+			model.addAttribute("isDetail", true);
+			return "page/customer";
+		} else {
+			return showCustomerList(model);
+		}
 	}
 	
 	@GetMapping("/{id}")
-	public String showCustomer(@RequestParam int id, Model model) {
-		Customer customerObj = customerRepo.getReferenceById(id);
-		model.addAttribute("customer", customerObj);
-		return "page/customer-detail";
+	public String getCustomer(@PathVariable("id") int id, Model model) {
+		Optional<Customer> customerObj = customerRepo.findById(id);
+		if (customerObj.isPresent()) {
+			model.addAttribute("customer", customerObj);
+			model.addAttribute("isUpdate", true);
+			return "page/customer";
+		} else {
+			return showCustomerList(model);
+		}
+	}
+
+	@PostMapping("/{id}")
+	public String updateCustomer(@PathVariable("id") int id,
+			@Valid @ModelAttribute("customer") Customer customer, Errors errors,
+			Model model) {
+		if (errors.hasErrors()) {
+			model.addAttribute("isUpdate", true);
+			return "page/customer";
+		} else {
+			customerRepo.save(customer);
+			model.addAttribute("isUpdate", true);
+			return "page/customer";
+		}
 	}
 	
+	@GetMapping("/delete/{id}")
+	public String deleteCustomer(@PathVariable("id") int id, Model model) {
+		customerRepo.deleteById(id);
+		return showCustomerList(model);
+	}
+
 	@GetMapping("/new-customer")
 	public String showCustomerForm(Model model) {
 		model.addAttribute("isNewCustomer", true);
 		return "page/customer";
 	}
-	
+
 	@PostMapping("/new-customer")
-	public String createCustomer(@Valid @ModelAttribute("customer") Customer customer, Errors errors, Model model) {
+	public String createCustomer(
+			@Valid @ModelAttribute("customer") Customer customer, Errors errors,
+			Model model) {
 		if (errors.hasErrors()) {
 			model.addAttribute("isNewCustomer", true);
 			return "page/customer";
 		} else {
 			customerRepo.save(customer);
-			return "redirect:/customer";
+			return showCustomerList(model);
 		}
 	}
 }
